3eda075666e613855cb81da01b7f756946d72783
Lalu Septiadi Badaruddin <38779364+lalusaptiadibadaruddin@users.noreply.github.com>
2026-02-23 14:29:01 +0700

    signup user api route

---
+++ backend/controller/auth.controller.js
@@ -0,0 +1,56 @@
+import User from "../models/user.model.js";
+import { errorHandler } from "../utils/error.js";
+import bcryptjs from "bcryptjs";
+
+export const signup = async (req, res) => {
+  const { name, email, password, profileImageUrl, adminJoinCode } = req.body;
+
+  if (
+    !name ||
+    !email ||
+    !password ||
+    name === "" ||
+    email === "" ||
+    password === ""
+  ) {
+    return res.status(400).json({ message: "All fields are required" });
+    // return next(errorHandler(400, "All fields are required"));
+  }
+
+  //   Check if user already exists
+
+  const isAlreadyExist = await User.findOne({ email });
+
+  if (isAlreadyExist) {
+    // return next(errorHandler(400, "User already exists"));
+    return res
+      .status(400)
+      .json({ success: false, message: "User already exists" });
+  }
+
+  //   check user role
+  let role = "user";
+
+  if (adminJoinCode && adminJoinCode === process.env.ADMIN_JOIN_CODE) {
+    role = "admin";
+  }
+
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+
+  const newUser = new User({
+    name,
+    email,
+    password: hashedPassword,
+    profileImageUrl,
+    role,
+  });
+
+  try {
+    await newUser.save();
+
+    res.json("Signup successful");
+  } catch (error) {
+    return res.status(500).json({ message: error.message() });
+    // next(error.message);
+  }
+};
