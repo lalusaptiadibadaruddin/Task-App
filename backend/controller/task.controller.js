6b169264c75292cfd35755c4b9169d28593ea6c7
Lalu Septiadi Badaruddin <38779364+lalusaptiadibadaruddin@users.noreply.github.com>
2026-02-24 12:50:01 +0700

    create task api route

---
+++ backend/controller/task.controller.js
@@ -0,0 +1,109 @@
+import mongoose from "mongoose";
+import { errorHandler } from "../utils/error.js";
+import Task from "../models/task.model.js";
+
+export const createTask = async (req, res, next) => {
+  try {
+    const {
+      title,
+      description,
+      priority,
+      dueDate,
+      assignedTo,
+      attachments,
+      todoChecklist,
+    } = req.body;
+
+    if (!Array.isArray(assignedTo)) {
+      return next(errorHandler(400, "assignedTo must be an array of user IDs"));
+    }
+
+    const task = await Task.create({
+      title,
+      description,
+      priority,
+      dueDate,
+      assignedTo,
+      attachments,
+      todoChecklist,
+      createdBy: req.user.id,
+    });
+
+    res.status(201).json({ message: "Task created successfully", task });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getTasks = async (req, res, next) => {
+  try {
+    const { status } = req.query;
+
+    let filter = {};
+
+    if (status) {
+      filter.status = status;
+    }
+
+    let tasks;
+    if (req.user.role === "admin") {
+      tasks = await Task.find(filter).populate(
+        "assignedTo",
+        "name email profileImageUrl",
+      );
+    } else {
+      tasks = await Task.find({
+        ...filter,
+        assignedTo: req.user.id,
+      }).populate("assignedTo", "name email profileImageUrl");
+    }
+
+    tasks = await Promise.all(
+      tasks.map(async (task) => {
+        const completedCount = task.todoChecklist.filter(
+          (item) => item.completed,
+        ).length;
+
+        return { ...task._doc, completedCount: completedCount };
+      }),
+    );
+
+    // status summary count
+
+    const allTasks = await Task.countDocuments(
+      req.user.role === "admin" ? {} : { assignedTo: req.user.id },
+    );
+
+    const pendingTasks = await Task.countDocuments({
+      ...filter,
+      status: "Pending",
+      //   if logged in user is not admin then add assignedTo filter
+      //  if logged in user is an admin then nothing to do, just count
+      ...(req.user.role !== "admin" && { assignedTo: req.user.id }),
+    });
+
+    const inProgressTasks = await Task.countDocuments({
+      ...filter,
+      status: "In Progress",
+      ...(req.user.role !== "admin" && { assignedTo: req.user.id }),
+    });
+
+    const completedTasks = await Task.countDocuments({
+      ...filter,
+      status: "Completed",
+      ...(req.user.role !== "admin" && { assignedTo: req.user.id }),
+    });
+
+    res.status(200).json({
+      tasks,
+      statusSummary: {
+        all: allTasks,
+        pendingTasks,
+        inProgressTasks,
+        completedTasks,
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
+};
---
+++ backend/routes/task.route.js
@@ -0,0 +1,25 @@
+import express from "express";
+import { adminOnly, verifyToken } from "../utils/verifyUser.js";
+import { createTask, getTasks } from "../controller/task.controller.js";
+
+const router = express.Router();
+
+router.post("/create", verifyToken, adminOnly, createTask);
+
+router.get("/", verifyToken, getTasks);
+
+// router.get("/dashboard-data", verifyToken, adminOnly, getDashboardData)
+
+// router.get("/user-dashboard-data", verifyToken, userDashboardData)
+
+// router.get("/:id", verifyToken, getTaskById)
+
+// router.put("/:id", verifyToken, updateTask)
+
+// router.delete("/:id", verifyToken, adminOnly, deleteTask)
+
+// router.put("/:id/status", verifyToken, updateTaskStatus)
+
+// router.put("/:id/todo", verifyToken, updateTaskChecklist)
+
+export default router;
