03542c42334d14833a55762a9ceea3e454867e0b
Lalu Septiadi Badaruddin <38779364+lalusaptiadibadaruddin@users.noreply.github.com>
2026-02-24 14:10:09 +0700

    update task api route

--- backend/routes/task.route.js
+++ backend/routes/task.route.js
@@ -1,6 +1,16 @@
 import express from "express";
 import { adminOnly, verifyToken } from "../utils/verifyUser.js";
-import { createTask, getTasks } from "../controller/task.controller.js";
+import {
+  createTask,
+  deleteTask,
+  getDashboardData,
+  getTaskById,
+  getTasks,
+  updateTask,
+  updateTaskChecklist,
+  updateTaskStatus,
+  userDashboardData,
+} from "../controller/task.controller.js";
 
 const router = express.Router();
 
@@ -8,18 +18,18 @@ router.post("/create", verifyToken, adminOnly, createTask);
 
 router.get("/", verifyToken, getTasks);
 
-// router.get("/dashboard-data", verifyToken, adminOnly, getDashboardData)
+router.get("/dashboard-data", verifyToken, adminOnly, getDashboardData);
 
-// router.get("/user-dashboard-data", verifyToken, userDashboardData)
+router.get("/user-dashboard-data", verifyToken, userDashboardData);
 
-// router.get("/:id", verifyToken, getTaskById)
+router.get("/:id", verifyToken, getTaskById);
 
-// router.put("/:id", verifyToken, updateTask)
+router.put("/:id", verifyToken, updateTask);
 
-// router.delete("/:id", verifyToken, adminOnly, deleteTask)
+router.delete("/:id", verifyToken, adminOnly, deleteTask);
 
-// router.put("/:id/status", verifyToken, updateTaskStatus)
+router.put("/:id/status", verifyToken, updateTaskStatus);
 
-// router.put("/:id/todo", verifyToken, updateTaskChecklist)
+router.put("/:id/todo", verifyToken, updateTaskChecklist);
 
 export default router;
