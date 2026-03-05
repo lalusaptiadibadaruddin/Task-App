073e7ac6bd5db32825fea60375f727a59116e489
Lalu Septiadi Badaruddin <38779364+lalusaptiadibadaruddin@users.noreply.github.com>
2026-03-05 12:41:43 +0700

    create recent tasks

---
+++ frontend/src/components/CustomBarChart.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CustomBarChart = () => {
+  return <div></div>;
+};
+
+export default CustomBarChart;
---
+++ frontend/src/components/CustomPieChart.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CustomPieChart = () => {
+  return <div></div>;
+};
+
+export default CustomPieChart;
